b086c4938fe5a52905f983e90a7f08d539cc81f3
Dakey42 <69747681+Dakey42@users.noreply.github.com>
2025-02-09 08:13:15 +0100

    Create collectors.sql

---
+++ backend/collectors.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.collectors (
+	collector_id uuid DEFAULT auth.uid() NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	"name" text DEFAULT ''::text NULL,
+	first_name text DEFAULT ''::text NULL,
+	last_name text DEFAULT ''::text NULL,
+	email text DEFAULT ''::text NULL,
+	CONSTRAINT collectors_pkey PRIMARY KEY (collector_id)
+);
+
+ALTER TABLE public.collectors ADD CONSTRAINT collectors_id_fkey FOREIGN KEY (collector_id) REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE;
